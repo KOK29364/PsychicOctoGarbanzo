64a4b0c08e6d7b20c0006291c0a711408ec8f0d8
YoelK <yoelkastro@gmail.com>
2019-02-27 17:18:55 +0300

    Did a lot of stuff

--- OctoGarbonzo/src/program/Program.java
+++ OctoGarbonzo/src/program/Program.java
@@ -1,33 +1,42 @@
 package program;
 
 import javafx.application.Application;
+
 import javafx.geometry.Dimension2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+
 import javafx.stage.Stage;
 import terrain.Grid;
 
 public class Program extends Application{
 	
-	private final double PANE_WIDTH = 1024;
-	private final double PANE_HEIGHT = 720;
+	private static final double PANE_WIDTH = 1024; // Width of the window
+	private static final double PANE_HEIGHT = 720; // Height of the window
 	
-	Grid g = new Grid(new Dimension2D(7, 7), 50);
+	private static Grid g = new Grid(new Dimension2D(5, 5), 50); // Create a hexagonal Grid
+	
+	private static Canvas c; // Create the Canvas
+	
+	Timer t = new Timer(); // Create a new Timer to count track the frames
 
 	public static void main(String[] args){
-		launch(args);
+		launch(args); // Launch the Program
 	}
 	
+	/* Start the Program */
 	public void start(Stage stage) throws Exception {
 		
 //		stage.setFullScreen(true);
 //		stage.setMaximized(true);
 		
-//		this.g.setTileOrientation(Tile.FLAT);
+		System.out.println("y");
 		
-		Canvas c = new Canvas(this.PANE_WIDTH, this.PANE_HEIGHT);
+		g.center(0, 0, PANE_WIDTH, PANE_HEIGHT);
+		
+		c = new Canvas(PANE_WIDTH, PANE_HEIGHT);
 		
 		Pane root = new Pane();
 		root.getChildren().add(c);
@@ -38,12 +47,23 @@ public class Program extends Application{
 		
 		stage.show();
 		
-		this.draw(c.getGraphicsContext2D());
+		t.addKeyFrame(ae -> {tick();});
+		t.start();
 		
 	}
+	/* _ _ _ _ _ _ _ _ */
 	
-	public void draw(GraphicsContext gc){
+	/* The actions to do each frame, such as updating the screen */
+	private static void tick(){ 
+		g.tick();
+		draw(c.getGraphicsContext2D());
+	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
+	
+	/* Updates the screen by redrawing all objects */
+	public static void draw(GraphicsContext gc){ 
 		g.draw(gc);
 	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
 
 }
--- OctoGarbonzo/src/terrain/Grid.java
+++ OctoGarbonzo/src/terrain/Grid.java
@@ -2,94 +2,115 @@ package terrain;
 
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Point2D;
+
 import javafx.scene.canvas.GraphicsContext;
 
 public class Grid {
 
-	private Point2D location;
-	private Tile[][] tiles;
-	private double tileSize;
-	private Dimension2D tileDimensions;
-	private boolean tileOrientation;
+	private Point2D location; // The coordinates of the top left corner of the Grid on a window
+	private Tile[][] tiles; // The array representation of the Tiles forming the Grid
 	
+	/* Initialize a new Grid with a specified number of Tiles and size for the Tiles */
 	public Grid(Dimension2D dimensions, double tileSize){
-		
-		this.tileOrientation = Tile.POINTY;
-		
-		this.tileSize = tileSize;
+	
 		this.tiles = new Tile[(int) dimensions.getWidth()][(int) dimensions.getHeight()];
 		
-		double w = (this.tileOrientation)? Math.sqrt(3) * this.tileSize:2 * this.tileSize;
-		double h = (this.tileOrientation)? 2 * this.tileSize:Math.sqrt(3) * this.tileSize;
+		double w = Math.sqrt(3) * tileSize;
+		double h = 2 * tileSize;
 		
-		this.tileDimensions = new Dimension2D(w, h);
-		
-		this.location = new Point2D(w, h);
+		this.location = new Point2D(0, 0);
 		
 		for(int x = 0; x < tiles.length; x ++){
 			for(int y = 0; y < tiles[x].length; y ++){
-					tiles[x][y] = new Tile(new Point2D((w + (x * ((this.tileOrientation == Tile.POINTY)? 2:1.5) * w) + ((this.tileOrientation == Tile.POINTY)? (w * (y % 2)):0)) / 2, (h + (y * ((this.tileOrientation  == Tile.POINTY)? 1.5:2) * h) + ((this.tileOrientation == Tile.POINTY)? 0:h * (x % 2))) / 2), this.tileSize);
+					tiles[x][y] = new Tile(new Point2D((w + (x * 2 * w) + ((w * (y % 2)))) / 2, (h + (y * (1.5) * h)) / 2), tileSize, Tile.POINTY);
 			}
 		}
 		
 	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
 	
-	public Grid(Point2D location, Dimension2D dimensions, double tileSize){
+	/* Returns a String representation of the Grid */
+	public String toString(){ 
+		return "Grid [width = " + this.getSize().getWidth() + ", height = " +  this.getSize().getWidth() + "]";
+	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
+	
+	/* Change the orientation of the Tiles to the one given */
+	public void setTileOrientation(boolean tileOrientation){ 
 		
-		this.tileOrientation = Tile.POINTY;
+		double tileSize = tiles[0][0].getSize();
 		
-		this.tileSize = tileSize;
-		this.tiles = new Tile[(int) dimensions.getWidth()][(int) dimensions.getHeight()];
-		
-		double w = (this.tileOrientation)? Math.sqrt(3) * this.tileSize:2 * this.tileSize;
-		double h = (this.tileOrientation)? 2 * this.tileSize:Math.sqrt(3) * this.tileSize;
-		
-		this.tileDimensions = new Dimension2D(w, h);
-		
-		this.location = new Point2D(w + location.getX(), h + location.getY());
+		double w = (tileOrientation)? Math.sqrt(3) * tileSize:2 * tileSize;
+		double h = (tileOrientation)? 2 * tileSize:Math.sqrt(3) * tileSize;
 		
 		for(int x = 0; x < tiles.length; x ++){
 			for(int y = 0; y < tiles[x].length; y ++){
-				tiles[x][y] = new Tile(new Point2D((w + (x * ((this.tileOrientation)? 2:1.5) * w) + ((this.tileOrientation)? (w * (y % 2)):0)) / 2, (h + (y * ((this.tileOrientation)? 1.5:2) * h) + ((this.tileOrientation)? 0:h * (x % 2))) / 2), this.tileSize);
-
+				tiles[x][y].changeOrientation(tileOrientation);
+				
+				tiles[x][y].shift(((w + (x * ((tileOrientation)? 2:1.5) * w) + ((tileOrientation)? (w * (y % 2)):0)) / 2) - tiles[x][y].getLocation().getX(),
+									(h + (y * ((tileOrientation)? 1.5:2) * h) + ((tileOrientation)? 0:h * (x % 2))) / 2 - tiles[x][y].getLocation().getY());
 			}
 		}
 		
 	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
 	
-	public String toString(){
-		return "Grid [width = " + this.getSize().getWidth() + ", height = " +  this.getSize().getWidth() + ", tile orientation = " + ((this.tileOrientation == Tile.POINTY)? "Pointy":"Flat") + "]";
-	}
-	
-	public void setTileOrientation(boolean tileOrientation){
-		this.tileOrientation = tileOrientation;
-	}
-	
-	public double getTileSize(){
-		return this.tileSize;
-	}
-	
-	public Dimension2D getTileDimensions(){
-		return this.tileDimensions;
-	}
-	
-	public Point2D getLocation(){
+	/* Returns the current location of the Grid */
+	public Point2D getLocation(){ 
 		return this.location;
 	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
 	
-	public Dimension2D getSize(){
-		double w = this.tileDimensions.getWidth();
-		double h = this.tileDimensions.getHeight();
-		return new Dimension2D((this.tiles.length * w) + ((this.tileOrientation)? w:0), (this.tiles[0].length * h) + ((this.tileOrientation)? 0:h));
+	/* Returns the width and height of the entire Grid */
+	public Dimension2D getSize(){ 
+		
+		double w = this.tiles[0][0].getDimensions().getWidth();
+		double h = this.tiles[0][0].getDimensions().getHeight();
+		
+		return new Dimension2D((this.tiles.length * w) + ((this.tiles[0][0].getOrientation())? w / 2:0),
+				(this.tiles[0].length / 2 * h * 1.75) + ((this.tiles[0][0].getOrientation())? 0:h / 2));
+	
 	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
 	
-	public void draw(GraphicsContext gc){
+	/* Moves the Grid to the defined Point */
+	public void setLocation(Point2D loc){ 
+		
+		double deltaX = loc.getX();
+		double deltaY = loc.getY() - 10;
+		
+		this.location = loc; 
+		
 		for(int x = 0; x < this.tiles.length; x ++){
 			for(int y = 0; y < this.tiles[x].length; y ++){
-				if(this.tiles[x][y] != null)
-					this.tiles[x][y].draw(gc, this.tileOrientation);
+				this.tiles[x][y].shift(deltaX, deltaY);
 			}
 		}
+		
+	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
+	
+	/* Moves the whole Grid the the center of the rectangle defined	*/
+	public void center(int x, int y, double width, double height){ 
+		this.setLocation(new Point2D((width - this.getSize().getWidth()) / 2 + x, (height - this.getSize().getHeight()) / 2 + y));	
+	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
+
+	/* Draws all the Tiles the Grid contains */
+	public void draw(GraphicsContext gc){ 
+		
+		for(int x = 0; x < this.tiles.length; x ++){
+			for(int y = 0; y < this.tiles[x].length; y ++){
+				if(this.tiles[x][y] != null) 
+					this.tiles[x][y].draw(gc);
+			}
+		}	
+	}
+	/* _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ */
+	
+	
+	
+	public void tick(){
 	}
 	
 }
